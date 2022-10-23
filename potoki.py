2c3a881c3ac14f9b6b87928f971c74a2a137144c
I7eTp <116294989+I7eTp@users.noreply.github.com>
2022-10-23 17:43:39 +0500

    Пройдены условные операторы и операторы сравнения Часть2

--- potoki.py
+++ potoki.py
@@ -1,20 +1,14 @@
-if True:
-    print('if')
-elif True:
-    print('elif')
+x = 5
+
+if x == 5.5:
+    x = 1
+    print('Х был равный Нулю')
+
+elif type(x) == type(5) or type(x) == type(5.5):
+    print('X допустимое значение')
+
 else:
-    print('else')
+    print('X не допустимый тип данных')
+    x = 1
 
-x = -3
-if x == 0:
-    print('равен 0')
-elif x > 0:
-    print('больше 0')
-else:
-    print('меньше 0')
-
-x = 0
-if x == 0:
-    x = x - 2    # x += 1
-
-print(5/x)
+print(100/x)
